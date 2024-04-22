d65cd33e04128d9e452edc02bf75db53141f9da7
ghostinajar <markwellsed@gmail.com>
2024-04-22 03:58:25 +0000

    move index.js out of public

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  res.sendFile(join(__dirname, 'public/index.html'));
 });
 
 io.on('connection', (socket) => {
