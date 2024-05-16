508295103eb811c71aa94419f35f3cbcf5f99b69
Mark Wells <markwellsed@gmail.com>
2024-05-16 13:30:39 -0700

    -begin modeling users with GameWorld, UserManager, UserRepository

---
+++ model/classes/User.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(storedUser) {
+    this.name = storedUser.name;
+
+  }
+
+  
+}
--- model/classes/UserManager.js
+++ model/classes/UserManager.js
@@ -1,19 +1,8 @@
 import UserRepository from './UserRepository.js';
 
-class GameWorld {
+class UserManager {
     constructor() {
         this.userInstances = new Map();  // Stores all zones by their unique ID
         this.userRepository = new UserRepository(); //data access layer
-    }
-
-    addZone(zone) {
-        this.zones.set(zone.id, zone);
-    }
-
-    getZoneById(id) {
-        return this.zones.get(id);
-    }
+    };
 }
-
-
-
