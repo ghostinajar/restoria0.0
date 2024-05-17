6060016ffad836ae61662a7a825d5cf194874893
Mark Wells <markwellsed@gmail.com>
2024-05-17 12:45:19 -0700

    implemented and checked retrieveStoredUserByUsername, instantiateUserByUsername, updateStoreduser, createStoredUser, deleteStoredUserById methods in UserRepository

--- model/classes/UserRepository.js
+++ model/classes/UserRepository.js
@@ -1,5 +1,5 @@
 import User from "./User";
-import StoredUser from "../data_access/StoredUser";
+import { StoredUser } from "../data_access/StoredUser";
 
 /* Data access layer for User objects. 
 Can retrieve, instantiate, update, create, delete StoredUser records from db */
@@ -8,37 +8,61 @@ class UserRepository {
     constructor() {
     }
 
-    async retrieveStoredUser(username) {
+    async retrieveStoredUserByUsername(username) {
         try {
-            const storedUser = await this.User.findOne({ username});
+            const storedUser = await StoredUser.findOne({ username });
             if (!storedUser) {
                 throw new Error(`Couldn't find storedUser with username: "${username}" in the db.`);
             }
             return storedUser;
         } catch (error) {
             console.log(error);
+            throw error;
         }
     }
     
-    async instantiateUser(username) {
+    async instantiateUserByUsername(username) {
         try {
-            this.retrieveStoredUser(username);
+            const storedUser = await this.retrieveStoredUserByUsername(username);
             const user = new User(storedUser);
             if (!user) {
                 throw new Error(`Couldn't make a User from the StoredUser with username: "${username}".`);
             }
-            return new User(storedUser);
+            return user;
 
         } catch (error) {
             console.log(error);
+            throw error;
         }
     }
     
-    async updateStoredUser(id, updatedUser) {
-        return await this.User.findByIdAndUpdate(id, updatedUser, { new: true });
+    async updateStoredUser(user) {
+        try {
+            return await StoredUser.findByIdAndUpdate(user._id, user, { new: true });
+        } catch (error) {
+            console.log("Database returned an error on updateStoredUser:", error);
+            throw error;
+        }
+    }
+
+    async createStoredUser(newUser) {
+        try {
+            const storedUser = new StoredUser(newUser);
+            return await storedUser.save();
+        } catch (error) {
+            console.log("Database returned an error on createStoredUser:", error);
+            throw error; 
+        }
     }
     
-    async deleteStoredUser(id) {
-        return await this.User.findByIdAndDelete(id);
+    async deleteStoredUserById(id) {
+        try {
+            return await StoredUser.findByIdAndDelete(id); 
+        } catch (error) {
+            console.log("Database returned an error on deleteStoredUser:", error);
+            throw error; 
+        }
     }
-    }
+}
+
+export default UserRepository;
