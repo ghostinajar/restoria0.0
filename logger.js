47f2b4a30f262f26b22d5bc7c438c44f9d90f155
Mark Wells <markwellsed@gmail.com>
2024-05-19 20:40:55 -0700

    -move log files into logs folder and .gitignore it

--- logger.js
+++ logger.js
@@ -5,7 +5,7 @@ const { combine, timestamp, json, errors } = winston.format;
 
 // Create a transport to log to a file rotating daily and keep logs for 56 days
 const fileRotateTransport = new winston.transports.DailyRotateFile({
-    filename: 'combined-%DATE%.log',
+    filename: './logs/logcombined-%DATE%.log',
     datePattern: 'YYYY-MM-DD',
     maxFiles: '56d',
 });
@@ -30,10 +30,10 @@ const logger = winston.createLogger({
         new winston.transports.Console()
     ],
     exceptionHandlers: [
-        new winston.transports.File({ filename: 'exception.log' }),
+        new winston.transports.File({ filename: './logs/exception.log' }),
       ],
       rejectionHandlers: [
-        new winston.transports.File({ filename: 'rejections.log' }),
+        new winston.transports.File({ filename: './logs/rejections.log' }),
       ],
 });
 
