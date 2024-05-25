bf75fa8ae5bed9ea9ba98b3faa6e6519adf4ac2a
Mark Wells <markwellsed@gmail.com>
2024-05-25 13:28:59 -0700

    -apply min/max values to stats in StatBlock.js

--- model/classes/StatBlock.js
+++ model/classes/StatBlock.js
@@ -3,13 +3,48 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const statBlockSchema = new Schema({
-    strength: Number,
-    dexterity: Number,
-    constitution: Number,
-    intelligence: Number,
-    wisdom: Number,
-    charisma: Number,
-    spirit: Number,
+    strength: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    dexterity: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    constitution: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    intelligence: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    wisdom: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    charisma: { 
+        type: Number,
+        default: 10,
+        min: [12, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [18, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
+    spirit: { 
+        type: Number,
+        default: 0,
+        min: [-1000, 'The value of `{PATH}` (`{VALUE}`) is beneath the limit of `{MIN}`.'],
+        max: [1000, 'The value of `{PATH}` (`{VALUE}`) exceeds the limit of `{MAX}`.']
+    },
 }, { _id: false });
 
 export default statBlockSchema;
--- sampleData.js
+++ sampleData.js
@@ -7,9 +7,133 @@ import JOB from "./constants/JOB.js";
 import ROOM_TYPE from "./constants/ROOM_TYPE.js";
 import WEARABLE_LOCATION from "./constants/WEARABLE_LOCATION.js";
 
-export const testRoom = {
-    author: '664d9519c192a9fa0ffa2562',
-    fromZoneId: '664f8ca70cc5ae9b173969a8',
-    name: 'Duped coords room',
-    mapCoords: [49,39,0],
-}
+// export const rayu = {
+//     name: 'Rayu',
+//     pronouns: 1,
+//     location: {
+//         inZone: '664f8ca70cc5ae9b173969a8',
+//         inRoom: '',
+//         },
+//     job: 'mage',
+//     statBlock: {
+//         strength: 12,
+//         dexterity: 12,
+//         constitution: 12,
+//         intelligence: 14,
+//         wisdom: 12,
+//         charisma: 12,
+//         spirit: 0,
+//     },
+//     goldHeld: Number,
+//     goldBanked: Number,
+//     trainingPoints: Number,
+//     jobLevels: {
+//         cleric: Number,
+//         mage: Number,
+//         thief: Number,
+//         warrior: Number
+//     },
+//     description: {
+//         type: descriptionSchema,
+//         default: () => ({})
+//     },
+//     //TODO decide how to implement training. should name be one from a list of SPELLS constants?
+//     //should I merge command authorization with character ability authorization, where most commands
+//     //like create room have a default level 1, but trainable abilities like bash or cast fireball
+//     //can have higher levels which help calculate their effects?
+//     trained: {
+//         passives: [{
+//             name: String,
+//             level: Number
+//         }],
+//         spells: [{
+//             name: String,
+//             level: Number
+//         }],
+//     },
+//     inventory: {
+//         type: Map,
+//         of: {
+//           type: Schema.Types.ObjectId,
+//           ref: 'ItemInstance'
+//         }
+//     },
+//     equipped: {
+//         arms: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         body: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         ears: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         feet: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         finger1: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         finger2: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         hands: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         head: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         held: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         legs: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         neck: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         shield: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         shoulders: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         waist: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         wrist1: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         wrist2: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         weapon1: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },
+//         weapon2: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'ItemInstance'
+//         },               
+//     },
+//     affixes: [{
+//         type: affixSchema,
+//         default: () => ({})
+//     }],
+// }
