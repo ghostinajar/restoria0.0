c923453e3ffb2829bb04b3fdda83faf48a698170
Mark Wells <markwellsed@gmail.com>
2024-06-11 09:51:55 -0700

    Implement TS to who command

--- commands/who.ts
+++ commands/who.ts
@@ -1,9 +1,12 @@
 // who.js
+
 import logger from "../logger.js";
 import worldEmitter from "../model/classes/WorldEmitter.js";
 import makeMessage from "../types/makeMessage.js";
-async function who(user) {
-    logger.debug(`Who command initiated`);
+import { IUser } from "../model/classes/User.js";
+
+async function who(user : IUser) {
+    logger.debug(`Who command initiated`)
     const whoArray = await new Promise((resolve) => {
         worldEmitter.once('userManagerReturningWhoArray', resolve);
         worldEmitter.emit('requestingWhoArray');
@@ -12,8 +15,10 @@ async function who(user) {
         logger.error(`Error in who command.`);
         return;
     }
+
     let message = makeMessage(false, 'who', JSON.stringify(whoArray));
-    worldEmitter.emit(`messageFor${user.username}`, message);
-}
-;
+
+    worldEmitter.emit(`messageFor${user.username}`, message)
+};
+
 export default who;
