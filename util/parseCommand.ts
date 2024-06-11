d54ce244a6da25aa5458418f028ffc7dc08fc14a
Mark Wells <markwellsed@gmail.com>
2024-06-11 08:19:49 -0700

    Implement TS to validCommandWords and parseCommand

--- util/parseCommand.ts
+++ util/parseCommand.ts
@@ -1,8 +1,16 @@
 // parseCommand
 import { commandsWith1Param, commandsWith2Params } from "../constants/validCommandWords.js";
-function parseCommand(command) {
-    const parsedCommand = {
-        commandWord: ``,
+
+interface IParsedCommand {
+    commandWord : string;
+    directObject? : string;
+    indirectObject? : string;
+    string?: string;
+}
+
+function parseCommand (command: string) {
+    const parsedCommand : IParsedCommand = {
+        commandWord : ``,
     };
     const splitCommand = command.split(" ");
     parsedCommand.commandWord = splitCommand[0];
@@ -10,20 +18,18 @@ function parseCommand(command) {
         parsedCommand.directObject = splitCommand[1];
         if (splitCommand.length > 2) {
             parsedCommand.string = splitCommand.slice(2).join(" ");
-        }
-    }
-    else if (commandsWith2Params.find(word => word === parsedCommand.commandWord)) {
+            }
+    } else if (commandsWith2Params.find(word => word === parsedCommand.commandWord)) {
         parsedCommand.directObject = splitCommand[1];
         parsedCommand.indirectObject = splitCommand[2];
         if (splitCommand.length > 3) {
             parsedCommand.string = splitCommand.slice(3).join(" ");
-        }
-    }
-    else {
+            }
+    } else {
         parsedCommand.string = splitCommand.slice(1).join(" ");
     }
     //console.log(parsedCommand)
     return parsedCommand;
-}
-;
+};
+
 export default parseCommand;
