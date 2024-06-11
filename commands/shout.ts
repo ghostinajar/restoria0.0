07236f5d5fdacf3e31ed99b855d9c825f08f84f7
Mark Wells <markwellsed@gmail.com>
2024-06-11 09:33:25 -0700

    Implement TS to shout

--- commands/shout.ts
+++ commands/shout.ts
@@ -1,18 +1,25 @@
 // shout
+
 import logger from "../logger.js";
 import worldEmitter from '../model/classes/WorldEmitter.js';
 import makeMessage from "../types/makeMessage.js";
-function shout(parsedCommand, user) {
+import { IParsedCommand } from "../util/parseCommand.js";
+import { IUser } from "../model/classes/User.js";
+
+function shout(parsedCommand: IParsedCommand, user: IUser) {
     let message = makeMessage(true, 'shout', ``);
-    if (!parsedCommand.string) {
-        message.content = `Shout what?`;
-        worldEmitter.emit(`messageFor${user.username}`, message);
+    
+    if(!parsedCommand.string) {
+        message.content = `Shout what?`
+        worldEmitter.emit(`messageFor${user.username}`, message)
         return;
     }
+
     message.content = `You shout, "${parsedCommand.string}".`;
     worldEmitter.emit(`messageFor${user.username}`, message);
     message.content = `${user.name} shouts, "${parsedCommand.string}".`;
     worldEmitter.emit(`messageFor${user.username}sZone`, message);
-    logger.log(`comms`, `${user._id} ${user.name} shouted, "${parsedCommand.string}".`);
+    logger.log(`comms`,`${user._id} ${user.name} shouted, "${parsedCommand.string}".`)
 }
+
 export default shout;
