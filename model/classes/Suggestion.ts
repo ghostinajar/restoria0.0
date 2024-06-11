535e011e976c42561c455ad5480606ae36fb6929
Mark Wells <markwellsed@gmail.com>
2024-06-11 14:20:46 -0700

    Implement TS for Suggestion

--- model/classes/Suggestion.ts
+++ model/classes/Suggestion.ts
@@ -1,7 +1,18 @@
 import mongoose from 'mongoose';
-import historySchema from './History.js';
+import historySchema, { IHistory } from './History.js';
+
 const { Schema } = mongoose;
-const suggestionSchema = new Schema({
+
+export interface ISuggestion {
+    author: mongoose.Schema.Types.ObjectId;
+    refersToRoom: mongoose.Schema.Types.ObjectId;
+    refersToMobBlueprint: mongoose.Schema.Types.ObjectId;
+    refersToItemBlueprint: mongoose.Schema.Types.ObjectId;
+    body: string;
+    history: IHistory
+}
+
+const suggestionSchema = new Schema<ISuggestion>({
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
@@ -19,11 +30,12 @@ const suggestionSchema = new Schema({
     refersToItemBlueprint: {
         type: Schema.Types.ObjectId,
         ref: 'ItemBlueprint'
-    },
+    }, 
     body: String,
     history: {
         type: historySchema,
         default: () => ({})
     },
 });
+
 export default suggestionSchema;
