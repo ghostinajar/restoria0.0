601059f6607e5c78a47e0985d173bd5c7ce9e53f
Mark Wells <markwellsed@gmail.com>
2024-06-12 08:46:55 -0700

    Implement TS for isValidCommandWord

--- util/isValidCommandWord.ts
+++ util/isValidCommandWord.ts
@@ -1,13 +1,14 @@
 // isValidCommandWord
 import validCommandWords from '../constants/validCommandWords.js';
 import logger from '../logger.js';
+
 //TODO accept and log entire command object, not just commandWord
-function isValidCommandWord(commandWord) {
+function isValidCommandWord (commandWord: string) {
     if (!validCommandWords.includes(commandWord.toLowerCase())) {
         logger.error(`Server detected invalid commandWord: ${commandWord}`);
         return false;
     }
     return true;
-}
-;
+};
+
 export default isValidCommandWord;
