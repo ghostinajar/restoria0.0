add0ca155eb293d73b9cc431a9ed6c83579f8caa
Mark Wells <markwellsed@gmail.com>
2024-06-12 08:57:04 -0700

    Implement TS for resetPlayerLocation

--- util/resetPlayerLocation.ts
+++ util/resetPlayerLocation.ts
@@ -1,11 +1,14 @@
 // resetUserLocation
 import logger from "../logger.js";
+import User, { IUser } from "../model/classes/User.js";
 import mongoose from "mongoose";
-const resetUserLocation = async (user, message) => {
+
+const resetUserLocation = async (user : mongoose.Document & IUser, message: string) => {
     logger.error(message);
     user.location.inRoom = new mongoose.Types.ObjectId(process.env.WORLD_RECALL_ROOMID);
     user.location.inZone = new mongoose.Types.ObjectId(process.env.WORLD_RECALL_ZONEID);
     await user.save();
     return user.location;
-};
+}
+
 export default resetUserLocation;
