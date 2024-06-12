1e4eb94774fa1415a63c3fcf5d5241a04f3b41f2
Mark Wells <markwellsed@gmail.com>
2024-06-11 21:46:01 -0700

    Implement TS for MobManager, destroyMobs
    PREVIOUS COMMIT moved new _id handling for in-game mob instances to class constructor

--- model/classes/MobManager.ts
+++ model/classes/MobManager.ts
@@ -1,77 +1,71 @@
 import logger from '../../logger.js';
 import worldEmitter from './WorldEmitter.js';
-import Mob from './Mob.js';
+import mongoose from 'mongoose';
+import Mob, { IMob } from './Mob.js';
+import { IMobBlueprint } from './MobBlueprint.js';
+
 class MobManager {
     constructor() {
-        this.mobs = new Map(); // Stores all mobs with their _id.toString() as key
+        this.mobs = new Map();  // Stores all mobs with their _id.toString() as key
         worldEmitter.on('roomDestroyingMob', this.removingMobHandler);
         worldEmitter.on('roomRequestingNewMob', this.roomRequestingNewMobHandler);
-    }
-    ;
-    mobs;
-    roomRequestingNewMobHandler = async (blueprint) => {
-        if (!this.mobs) {
-            return null;
-        }
-        ;
+    };     
+
+    mobs : Map<string, IMob> | null;
+
+    roomRequestingNewMobHandler = async (blueprint : IMobBlueprint) => {
+        if (!this.mobs) {return null};
         // Create a copy of the blueprint and give its own unique Id
         const mob = new Mob(blueprint);
         this.mobs.set(mob._id.toString(), mob);
         logger.log(`debug`, `mobManager added ${mob.name} to mobs. Mobs after adding: ${JSON.stringify(Array.from(this.mobs.values()).map(mob => mob.name))}`);
         worldEmitter.emit('mobManagerAddedMob', mob);
-    };
-    removingMobHandler = async (mobId) => {
-        logger.debug(`removingMobHandler called...`);
-        logger.debug(`removingMobHandler removing mob with id: ${mobId}`);
+    }
+
+    removingMobHandler = async (mobId : string) => {
+        logger.debug(`removingMobHandler called...`)
+        logger.debug(`removingMobHandler removing mob with id: ${mobId}`)
         await this.removeMobById(mobId);
-        worldEmitter.emit(`mobManagerRemovedMob${mobId}`);
+        worldEmitter.emit(`mobManagerRemovedMob${mobId}`)
     };
-    async getMobById(id) {
+
+    async getMobById(id : string) {
         try {
-            if (!this.mobs) {
-                return null;
-            }
-            ;
+            if (!this.mobs) {return null};
             const mob = this.mobs.get(id.toString());
             if (mob) {
                 return mob;
-            }
-            else {
+            } else {
                 logger.error(`mobManager can't find mob with id: ${id}.`);
                 return null;
-            }
-            ;
-        }
-        catch (err) {
+            };
+        } catch(err : any) {
             logger.error(`Error in getMobById: ${err.message}`);
             throw err;
         }
     }
-    async removeMobById(id) {
+
+    async removeMobById(id : string) {
         try {
-            if (!this.mobs) {
-                return null;
-            }
-            ;
+            if (!this.mobs) {return null};
             logger.debug(`Removing mob with id "${id}"`);
             if (this.mobs.has(id)) {
                 this.mobs.delete(id);
-            }
-            else {
+            } else {
                 logger.warn(`Mob with id "${id}" does not exist.`);
             }
             //logger.info(`Mob Removed. Active mobs remaining: ${JSON.stringify(Array.from(this.mobs.values()).map(mob => mob.name))}`)
-        }
-        catch (err) {
+        } catch(err : any) {
             logger.error(`Error in removeMobById: ${err.message}`);
             throw err;
-        }
-        ;
+        };
     }
+
     clearContents() {
-        this.mobs = null;
+        this.mobs = null
         worldEmitter.off('roomDestroyingMob', this.removingMobHandler);
         worldEmitter.off('roomRequestingNewMob', this.roomRequestingNewMobHandler);
     }
 }
+
 export default MobManager;
--- util/destroyMobs.ts
+++ util/destroyMobs.ts
@@ -1,20 +1,22 @@
 import worldEmitter from "../model/classes/WorldEmitter.js";
 import logger from "../logger.js";
-async function destroyMobs(mobArray) {
+import { IMob } from "../model/classes/Mob.js";
+
+async function destroyMobs(mobArray : Array<IMob>) {
     for (const mob of mobArray) {
         try {
             mob.inventory = [];
+
             //logger.debug(`Destroying mob "${mob.name}"`)
             await new Promise((resolve) => {
                 worldEmitter.once(`mobManagerRemovedMob${mob._id.toString}`, resolve);
                 worldEmitter.emit(`roomDestroyingMob`, mob._id.toString());
             });
-        }
-        catch (err) {
+        } catch(err : any) {
             logger.error(`Error in destroyMobs: ${err.message}`);
-            throw (err);
+            throw(err);
         }
     }
-}
-;
+};
+
 export default destroyMobs;
