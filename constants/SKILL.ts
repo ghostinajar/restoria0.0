ceecc2bc7fe49c68276d7a44c2364e3cf4df8430
Mark Wells <markwellsed@gmail.com>
2024-10-25 10:18:28 -0700

    separate SKILL from SPELL constants

---
+++ constants/SKILL.ts
@@ -0,0 +1,30 @@
+const SKILL = {
+  NONE: "none",
+  AMBUSH: "ambush",
+  BACKSTAB: "backstab",
+  CLEAVE: "cleave",
+  DEFEND: "defend",
+  DISARM: "disarm",
+  DODGE: "dodge",
+  GUARD: "guard",
+  HIDE: "hide",
+  KICK: "kick",
+  MEDITATE: "meditate",
+  OFFHAND: "offhand",
+  PARRY: "parry",
+  PICK: "pick",
+  SCAN: "scan",
+  SECOND_ATTACK: "second attack",
+  SHOVE: "shove",
+  SNEAK: "sneak",
+  STEAL: "steal",
+  THIRD_ATTACK: "third attack",
+  THROW: "throw",
+  TRACK: "track",
+  TRAP: "trap",
+  TRIP: "trip",
+};
+
+export const skills = Object.values(SKILL);
+
+export default SKILL;
