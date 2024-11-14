f08dc4b3c517bb2d093da7d128a519f63d7658eb
Mark Wells <markwellsed@gmail.com>
2024-11-14 14:03:10 -0800

    improve error handling in getRoomOfUser

--- util/getRoomOfUser.js
+++ util/getRoomOfUser.js
@@ -1,9 +1,15 @@
 import worldEmitter from "../model/classes/WorldEmitter.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 async function getRoomOfUser(user) {
-    const room = await new Promise((resolve) => {
-        worldEmitter.once(`zoneManagerReturningRoom${user.location.inRoom.toString()}`, resolve);
-        worldEmitter.emit("roomRequested", user.location);
-    });
-    return room;
+    try {
+        const room = await new Promise((resolve) => {
+            worldEmitter.once(`zoneManagerReturningRoom${user.location.inRoom.toString()}`, resolve);
+            worldEmitter.emit("roomRequested", user.location);
+        });
+        return room;
+    }
+    catch (error) {
+        catchErrorHandlerForFunction("functionName", error);
+    }
 }
 export default getRoomOfUser;
--- util/getRoomOfUser.ts
+++ util/getRoomOfUser.ts
@@ -1,16 +1,21 @@
 import { IRoom } from "../model/classes/Room.js";
 import { IUser } from "../model/classes/User.js";
 import worldEmitter from "../model/classes/WorldEmitter.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 
 async function getRoomOfUser(user: IUser) {
-  const room: IRoom = await new Promise((resolve) => {
-    worldEmitter.once(
-      `zoneManagerReturningRoom${user.location.inRoom.toString()}`,
-      resolve
-    );
-    worldEmitter.emit("roomRequested", user.location);
-  });
-  return room;
+  try {
+    const room: IRoom = await new Promise((resolve) => {
+      worldEmitter.once(
+        `zoneManagerReturningRoom${user.location.inRoom.toString()}`,
+        resolve
+      );
+      worldEmitter.emit("roomRequested", user.location);
+    });
+    return room;
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction("functionName", error);
+  }
 }
 
 export default getRoomOfUser;
