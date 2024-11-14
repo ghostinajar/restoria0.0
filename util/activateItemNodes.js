94d035e6e6479c61ba3bdc1b0c3ea6b55db88a46
Mark Wells <markwellsed@gmail.com>
2024-11-14 11:55:29 -0800

    improve error handling in activateItemNoes

--- util/activateItemNodes.js
+++ util/activateItemNodes.js
@@ -2,51 +2,56 @@
 import worldEmitter from "../model/classes/WorldEmitter.js";
 import logger from "../logger.js";
 import createItemFromBlueprint from "./createItemFromBlueprint.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 async function activateItemNodes(itemNodes, inventory, isNested = false) {
-    for (const itemNode of itemNodes) {
-        try {
-            const zone = await new Promise((resolve) => {
-                worldEmitter.once(`zone${itemNode.fromZoneId.toString()}Loaded`, resolve);
-                worldEmitter.emit(`zoneRequested`, itemNode.fromZoneId.toString());
-            });
-            if (!zone) {
-                logger.error(`activateItemNodes couldn't get a zone`);
-                return null;
-            }
-            const blueprint = await zone.itemBlueprints.find((blueprint) => blueprint._id.toString() ===
-                itemNode.loadsBlueprintId.toString());
-            if (!blueprint) {
-                logger.error(`ActivateItemNodes couldn't find blueprint ${itemNode.loadsBlueprintId} in zone ${zone.name}.`);
-                return null;
-            }
-            const item = await createItemFromBlueprint(blueprint);
-            // If item is a container and we're not in a nested inventory,
-            // initiate its inventory recursively
-            if (item.itemType == "container" && !isNested) {
-                if (!item.inventory) {
-                    item.inventory = [];
+    try {
+        for (const itemNode of itemNodes) {
+            try {
+                const zone = await new Promise((resolve) => {
+                    worldEmitter.once(`zone${itemNode.fromZoneId.toString()}Loaded`, resolve);
+                    worldEmitter.emit(`zoneRequested`, itemNode.fromZoneId.toString());
+                });
+                if (!zone) {
+                    logger.error(`activateItemNodes couldn't get a zone`);
+                    return null;
                 }
-                if (blueprint.itemNodes) {
-                    await activateItemNodes(blueprint.itemNodes, item.inventory, true);
-                    // logger.debug(
-                    //   `Items in container "${item.name}": ${item.inventory.map(
-                    //     (item: IItem) => {
-                    //       return item.name;
-                    //     }
-                    //   )}`
-                    // );
+                const blueprint = await zone.itemBlueprints.find((blueprint) => blueprint._id.toString() === itemNode.loadsBlueprintId.toString());
+                if (!blueprint) {
+                    logger.error(`ActivateItemNodes couldn't find blueprint ${itemNode.loadsBlueprintId} in zone ${zone.name}.`);
+                    return null;
                 }
+                const item = await createItemFromBlueprint(blueprint);
+                // If item is a container and we're not in a nested inventory,
+                // initiate its inventory recursively
+                if (item.itemType == "container" && !isNested) {
+                    if (!item.inventory) {
+                        item.inventory = [];
+                    }
+                    if (blueprint.itemNodes) {
+                        await activateItemNodes(blueprint.itemNodes, item.inventory, true);
+                        // logger.debug(
+                        //   `Items in container "${item.name}": ${item.inventory.map(
+                        //     (item: IItem) => {
+                        //       return item.name;
+                        //     }
+                        //   )}`
+                        // );
+                    }
+                }
+                else if (item.itemType == "container" && isNested) {
+                    logger.error(`Skipping container "${item.name}" because it is nested.`);
+                    continue;
+                }
+                inventory.push(item);
             }
-            else if (item.itemType == "container" && isNested) {
-                logger.error(`Skipping container "${item.name}" because it is nested.`);
-                continue;
+            catch (err) {
+                logger.error(`Error in ActivateItemNodes with an itemNode: ${err.message}`);
+                throw err;
             }
-            inventory.push(item);
         }
-        catch (err) {
-            logger.error(`Error in ActivateItemNodes with an itemNode: ${err.message}`);
-            throw err;
-        }
+    }
+    catch (error) {
+        catchErrorHandlerForFunction("activateItemNodes", error);
     }
 }
 export default activateItemNodes;
