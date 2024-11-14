247ebf9fd2abf631257f46801690323599619139
Mark Wells <markwellsed@gmail.com>
2024-11-14 14:01:38 -0800

    improve error handling in getRoomNamesFromZone

--- util/getRoomNamesFromZone.js
+++ util/getRoomNamesFromZone.js
@@ -1,7 +1,13 @@
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 function getRoomNamesFromZone(zone) {
-    const roomNames = zone.rooms.map((room) => {
-        return { _id: room._id, name: room.name };
-    });
-    return roomNames;
+    try {
+        const roomNames = zone.rooms.map((room) => {
+            return { _id: room._id, name: room.name };
+        });
+        return roomNames;
+    }
+    catch (error) {
+        catchErrorHandlerForFunction("getRoomNamesFromZone", error);
+    }
 }
 export default getRoomNamesFromZone;
--- util/getRoomNamesFromZone.ts
+++ util/getRoomNamesFromZone.ts
@@ -1,11 +1,16 @@
 //getRoomNamesFromZone
 import { IZone } from "../model/classes/Zone.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 
 function getRoomNamesFromZone(zone: IZone) {
-  const roomNames = zone.rooms.map((room) => {
-    return { _id: room._id, name: room.name };
-  });
-  return roomNames;
+  try {
+    const roomNames = zone.rooms.map((room) => {
+      return { _id: room._id, name: room.name };
+    });
+    return roomNames;
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction("getRoomNamesFromZone", error);
+  }
 }
 
 export default getRoomNamesFromZone;
