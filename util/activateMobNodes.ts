31a227a5e56b093626fd10edad36347b8b7b34f3
Mark Wells <markwellsed@gmail.com>
2024-11-14 11:57:09 -0800

    improve error handling for activateMobNodes

--- util/activateMobNodes.ts
+++ util/activateMobNodes.ts
@@ -6,52 +6,59 @@ import { IMobNode } from "../model/classes/MobNode.js";
 import { IMob } from "../model/classes/Mob.js";
 import { IMobBlueprint } from "../model/classes/MobBlueprint.js";
 import { IZone } from "../model/classes/Zone.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 
 async function activateMobNodes(
   mobNodes: Array<IMobNode>,
   mobArray: Array<IMob>
 ) {
-  for (const mobNode of mobNodes) {
-    try {
-      const zone: IZone = await new Promise((resolve) => {
-        worldEmitter.once(
-          `zone${mobNode.fromZoneId.toString()}Loaded`,
-          resolve
-        );
-        worldEmitter.emit(`zoneRequested`, mobNode.fromZoneId.toString());
-      });
-      if (!zone) {
-        logger.error(`activateMobNodes couldn't get a zone`);
-        return null;
-      }
+  try {
+    for (const mobNode of mobNodes) {
+      try {
+        const zone: IZone = await new Promise((resolve) => {
+          worldEmitter.once(
+            `zone${mobNode.fromZoneId.toString()}Loaded`,
+            resolve
+          );
+          worldEmitter.emit(`zoneRequested`, mobNode.fromZoneId.toString());
+        });
+        if (!zone) {
+          logger.error(`activateMobNodes couldn't get a zone`);
+          return null;
+        }
 
-      const blueprint = await zone.mobBlueprints.find(
-        (blueprint: IMobBlueprint) =>
-          blueprint._id.toString() === mobNode.loadsBlueprintId.toString()
-      );
-      if (!blueprint) {
+        const blueprint = await zone.mobBlueprints.find(
+          (blueprint: IMobBlueprint) =>
+            blueprint._id.toString() === mobNode.loadsBlueprintId.toString()
+        );
+        if (!blueprint) {
+          logger.error(
+            `activateMobNodes couldn't find blueprint ${mobNode.loadsBlueprintId} in zone ${zone.name}.`
+          );
+          return null;
+        }
+
+        const mob: IMob = await new Promise((resolve) => {
+          worldEmitter.once(
+            `mobManagerAddedMobFromBlueprint${blueprint._id}`,
+            resolve
+          );
+          worldEmitter.emit(`roomRequestingNewMob`, blueprint);
+        });
+        //TODO initiate mob inventory
+        mob.inventory = [];
+        await activateItemNodes(blueprint.itemNodes, mob.inventory);
+        //logger.debug(`Items in mob "${mob.name}": ${mob.inventory.map(item => item.name)}`)
+        await mobArray.push(mob);
+      } catch (err: any) {
         logger.error(
-          `activateMobNodes couldn't find blueprint ${mobNode.loadsBlueprintId} in zone ${zone.name}.`
+          `Error in activateMobNodes with a mobNode: ${err.message}`
         );
-        return null;
+        throw err;
       }
-
-      const mob: IMob = await new Promise((resolve) => {
-        worldEmitter.once(
-          `mobManagerAddedMobFromBlueprint${blueprint._id}`,
-          resolve
-        );
-        worldEmitter.emit(`roomRequestingNewMob`, blueprint);
-      });
-      //TODO initiate mob inventory
-      mob.inventory = [];
-      await activateItemNodes(blueprint.itemNodes, mob.inventory);
-      //logger.debug(`Items in mob "${mob.name}": ${mob.inventory.map(item => item.name)}`)
-      await mobArray.push(mob);
-    } catch (err: any) {
-      logger.error(`Error in activateMobNodes with a mobNode: ${err.message}`);
-      throw err;
     }
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction("activateMobNodes", error);
   }
 }
 
