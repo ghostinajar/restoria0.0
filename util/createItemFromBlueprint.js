8b79707d339a81604b02985cf683d3954950b2cf
Mark Wells <markwellsed@gmail.com>
2024-11-14 12:04:48 -0800

    improve error handling in createItemFromBlueprint

--- util/createItemFromBlueprint.js
+++ util/createItemFromBlueprint.js
@@ -1,13 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 async function createItemFromBlueprint(blueprint) {
-    // Create a copy of the blueprint and give its own unique Id
-    const item = JSON.parse(JSON.stringify(blueprint));
-    item._id = new mongoose.Types.ObjectId();
-    item.history = {
-        creationDate: Date.now,
-    };
-    item.itemNodes = null;
-    //logger.log(`loadout`, `createItemFromBlueprint: ${item.name}`);
-    return item;
+    try {
+        // Create a copy of the blueprint and give its own unique Id
+        const item = JSON.parse(JSON.stringify(blueprint));
+        item._id = new mongoose.Types.ObjectId();
+        item.history = {
+            creationDate: Date.now,
+        };
+        item.itemNodes = null;
+        return item;
+    }
+    catch (error) {
+        catchErrorHandlerForFunction("createItemFromBlueprint", error);
+    }
 }
 export default createItemFromBlueprint;
