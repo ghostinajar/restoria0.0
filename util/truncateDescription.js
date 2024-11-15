be832d9c8534a7d49216cfdf2010e0765e916ecc
Mark Wells <markwellsed@gmail.com>
2024-11-15 14:50:50 -0800

    improve error handling in truncateDescription

--- util/truncateDescription.js
+++ util/truncateDescription.js
@@ -1,22 +1,34 @@
 import worldEmitter from "../model/classes/WorldEmitter.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 import makeMessage from "./makeMessage.js";
 function truncateDescription(description, user) {
-    if (description.look && description.look.length > 80) {
-        description.look = description.look.substring(0, 80);
-        worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Look descriptions should be 80 characters (1 lines) or less. Shortened to '${description.look}'.`));
+    try {
+        if (description.look && description.look.length > 80) {
+            description.look = description.look.substring(0, 80);
+            worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Look descriptions should be 80 characters (1 lines) or less. Shortened to '${description.look}'.`));
+        }
+        if (description.examine && description.examine.length > 240) {
+            description.examine = description.examine.substring(0, 240);
+            worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Examine descriptions should be 240 characters (3 lines) or less. Shortened to '${description.examine}'.`));
+        }
+        if (description.study && description.study.length > 640) {
+            description.study = description.study.substring(0, 640);
+            worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Study descriptions should be 640 characters (8 lines) or less. Shortened to '${description.study}'.`));
+        }
+        if (description.research && description.research.length > 1600) {
+            description.research = description.research.substring(0, 1600);
+            worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Research descriptions should be 1600 characters (20 lines) or less. Shortened to '${description.research}'.`));
+        }
+        return description;
     }
-    if (description.examine && description.examine.length > 240) {
-        description.examine = description.examine.substring(0, 240);
-        worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Examine descriptions should be 240 characters (3 lines) or less. Shortened to '${description.examine}'.`));
+    catch (error) {
+        catchErrorHandlerForFunction(`truncateDescription`, error, user?.name);
+        return {
+            look: '',
+            examine: '',
+            study: '',
+            research: '',
+        };
     }
-    if (description.study && description.study.length > 640) {
-        description.study = description.study.substring(0, 640);
-        worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Study descriptions should be 640 characters (8 lines) or less. Shortened to '${description.study}'.`));
-    }
-    if (description.research && description.research.length > 1600) {
-        description.research = description.research.substring(0, 1600);
-        worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `Research descriptions should be 1600 characters (20 lines) or less. Shortened to '${description.research}'.`));
-    }
-    return description;
 }
 export default truncateDescription;
