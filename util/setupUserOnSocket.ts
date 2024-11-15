3385df5f5e4f855d85a403c09c393acf63ab7438
Mark Wells <markwellsed@gmail.com>
2024-11-15 14:48:45 -0800

    improve error handling in setupUserOnSocket

--- util/setupUserOnSocket.ts
+++ util/setupUserOnSocket.ts
@@ -2,12 +2,16 @@
 import { IUser } from "../model/classes/User.js";
 import worldEmitter from "../model/classes/WorldEmitter.js";
 import logger from "../logger.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 
 async function setupUserOnSocket(socket: any) {
   try {
     // Get user, alert userManager
     const user: IUser = await new Promise((resolve) => {
-      worldEmitter.once(`userManagerAddedUser${socket.request.session.passport.user._id}`, resolve);
+      worldEmitter.once(
+        `userManagerAddedUser${socket.request.session.passport.user._id}`,
+        resolve
+      );
       worldEmitter.emit(
         `socketConnectingUser`,
         socket.request.session.passport.user._id
@@ -25,8 +29,8 @@ async function setupUserOnSocket(socket: any) {
     socket.join(user.location.inRoom.toString());
     socket.join(user.location.inZone.toString());
     return user;
-  } catch (err: any) {
-    logger.error(`Error in setupUserOnSocket: ${err.message}`);
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction(`setupUserOnSocket`, error);
   }
 }
 
