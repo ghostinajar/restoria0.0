34464531608c28d7429cf700c992ba476d3ff3a5
Mark Wells <markwellsed@gmail.com>
2024-11-15 14:36:29 -0800

    improve error handling in purify

--- util/purify.ts
+++ util/purify.ts
@@ -1,67 +1,90 @@
 import { JSDOM } from "jsdom";
 import DOMPurify from "dompurify";
 import { Filter } from "bad-words";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 
 const window = new JSDOM("").window;
 const purify = DOMPurify(window);
 export const wordFilter = new Filter();
 
 export function purifyCommandInput(input: string): string {
-  let sanitizedInput = purify.sanitize(input);
-  return wordFilter.clean(sanitizedInput);
+  try {
+    let sanitizedInput = purify.sanitize(input);
+    return wordFilter.clean(sanitizedInput);
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction(`functionName`, error);
+    return "purify-command-input-error";
+  }
 }
 
 export function purifyAllStringPropsOfObject(object: any) {
-  if (object && typeof object === 'object') {
-    for (const key in object) {
-      if (typeof object[key] === 'string') {
-        object[key] = wordFilter.clean(purify.sanitize(object[key]));
-      } else if (Array.isArray(object[key])) {
-        object[key] = object[key].map(item =>
-          typeof item === 'string' ? wordFilter.clean(purify.sanitize(item)) : purifyAllStringPropsOfObject(item)
-        );
-      } else if (typeof object[key] === 'object') {
-        purifyAllStringPropsOfObject(object[key]);
+  try {
+    if (object && typeof object === "object") {
+      for (const key in object) {
+        if (typeof object[key] === "string") {
+          object[key] = wordFilter.clean(purify.sanitize(object[key]));
+        } else if (Array.isArray(object[key])) {
+          object[key] = object[key].map((item) =>
+            typeof item === "string"
+              ? wordFilter.clean(purify.sanitize(item))
+              : purifyAllStringPropsOfObject(item)
+          );
+        } else if (typeof object[key] === "object") {
+          purifyAllStringPropsOfObject(object[key]);
+        }
       }
     }
+    return object;
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction(`functionName`, error);
+    return object;
   }
-  return object;
 }
 
 export function purifyDescriptionOfObject(object: any) {
-  if (object.name) {
-    object.name = wordFilter.clean(purify.sanitize(object.name));
+  try {
+    if (object.name) {
+      object.name = wordFilter.clean(purify.sanitize(object.name));
+    }
+  
+    if (object.keywords) {
+      object.keywords = object.keywords.map((keyword: string) =>
+        wordFilter.clean(purify.sanitize(keyword))
+      );
+    }
+  
+    if (object.description) {
+      if (object.description.look) {
+        object.description.look = wordFilter.clean(
+          purify.sanitize(object.description.look)
+        );
+      }
+      if (object.description.examine) {
+        object.description.examine = wordFilter.clean(
+          purify.sanitize(object.description.examine)
+        );
+      }
+      if (object.description.study) {
+        object.description.study = wordFilter.clean(
+          purify.sanitize(object.description.study)
+        );
+      }
+      if (object.description.research) {
+        object.description.research = wordFilter.clean(
+          purify.sanitize(object.description.research)
+        );
+      }
+    }
+    return object;
+  } catch (error: unknown) {
+    catchErrorHandlerForFunction(`functionName`, error);
+    return {
+      look: '',
+      examine: '',
+      study: '',
+      research: '',
+    }
   }
-
-  if (object.keywords) {
-    object.keywords = object.keywords.map((keyword: string) =>
-      wordFilter.clean(purify.sanitize(keyword))
-    );
-  }
-
-  if (object.description) {
-    if (object.description.look) {
-      object.description.look = wordFilter.clean(
-        purify.sanitize(object.description.look)
-      );
-    }
-    if (object.description.examine) {
-      object.description.examine = wordFilter.clean(
-        purify.sanitize(object.description.examine)
-      );
-    }
-    if (object.description.study) {
-      object.description.study = wordFilter.clean(
-        purify.sanitize(object.description.study)
-      );
-    }
-    if (object.description.research) {
-      object.description.research = wordFilter.clean(
-        purify.sanitize(object.description.research)
-      );
-    }
-  }
-  return object;
 }
 
 export default purifyDescriptionOfObject;
