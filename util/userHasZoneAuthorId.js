e77324e3b5d8689deb0fb5b100dba9f271991a6e
Mark Wells <markwellsed@gmail.com>
2024-11-15 14:58:13 -0800

    improve error handling in userHasZoneAuthorId

--- util/userHasZoneAuthorId.js
+++ util/userHasZoneAuthorId.js
@@ -1,11 +1,17 @@
-// userHasZoneAuthorId
 import worldEmitter from "../model/classes/WorldEmitter.js";
+import catchErrorHandlerForFunction from "./catchErrorHandlerForFunction.js";
 import makeMessage from "./makeMessage.js";
 function userHasZoneAuthorId(zoneAuthorId, user) {
-    if (zoneAuthorId !== user._id.toString()) {
-        worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `You aren't an author for this zone.`));
+    try {
+        if (zoneAuthorId !== user._id.toString()) {
+            worldEmitter.emit(`messageFor${user.username}`, makeMessage(`rejection`, `You aren't an author for this zone.`));
+            return false;
+        }
+        return true;
+    }
+    catch (error) {
+        catchErrorHandlerForFunction(`userHasZoneAuthorId`, error, user?.name);
         return false;
     }
-    return true;
 }
 export default userHasZoneAuthorId;
