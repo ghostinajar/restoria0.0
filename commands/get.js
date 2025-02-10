5e9d806f78b80de7ecaa3d0255fb2ea4ae485b08
Mark Wells <markwellsed@gmail.com>
2025-02-10 15:02:09 -0800

    begin documenting plan in get.ts

--- commands/get.js
+++ commands/get.js
@@ -1,10 +1,25 @@
 // get
 // user can get objects from the ground or from containers
 import catchErrorHandlerForFunction from "../util/catchErrorHandlerForFunction.js";
+import messageToUsername from "../util/messageToUsername.js";
 async function get(parsedCommand, user) {
     try {
         console.log(`get command executed by ${user?.name}`);
         console.log(`parsedCommand:`, parsedCommand);
+        let containerKeyword = parsedCommand.indirectObject || `noIndirectObject`;
+        let targetContainer;
+        // identify container in user's inventory else in room inventory
+        targetContainer = user.inventory.find((item) => item.keywords.includes(containerKeyword));
+        // fail and notify user if container is a mob (ca
+        // fail and notify user if container isn't present
+        if (!targetContainer) {
+            messageToUsername(user.name, `You can't seem to find ${parsedCommand.indirectObject} here.`);
+            return;
+        }
+        // fail and notify user if target isn't present in container or room
+        // identify target object in container
+        // add target to player inventory
+        // remove target from room or container
     }
     catch (error) {
         catchErrorHandlerForFunction(`get`, error, user?.name);
--- commands/get.ts
+++ commands/get.ts
@@ -9,6 +9,21 @@ async function get(parsedCommand: IParsedCommand, user: IUser) {
   try {
     console.log(`get command executed by ${user?.name}`);
     console.log(`parsedCommand:`, parsedCommand);
+    // if no directObject specified, fail and notify user they need to specify what to get
+    // find the inventory to get from:
+      // if container is not specified, succeed with room.inventory
+      // if container is specified:
+        // if container is in user inventory, succeed with user.inventory
+        // if container is in room.inventory:
+          // if indirectObjectOrdinal is not specified, succeed with container.inventory of the first match in room.inventory
+          // if indirectObjectOrdinal is specified, get matchingContainers from room.inventory, and succeed with container.inventory of the matchingContainers[ordinal]
+        // if container is in room.mobs, fail and notify user not to get from mobs
+        // at this point we can fail and notify user they can't find the container to look in
+
+    // find the target object(all) in container
+      // 
+    // add target to player inventory
+    // remove target from room or container
   } catch (error: unknown) {
     catchErrorHandlerForFunction(`get`, error, user?.name);
   }
